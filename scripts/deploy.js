8bb475a14c6d85ae33a247e9d76df62be05847e7
Qiang Zhu <freedostudio@gmail.com>
2023-10-17 11:46:41 +0800

    more comments

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -9,7 +9,7 @@ async function main() {
       17, // maxKvSizeBits, 131072
       30, // shardSizeBits ~ 1G
       2, // randomChecks
-      10000000, // minimumDiff 10000000 / 60 = 16,666 sample/s is enable to mine, and one sAX101 can provide 1M/12 = 83,333 sample/s power
+      10000000, // minimumDiff 10000000 / 60 = 16,666 sample/s is enable to mine, and one AX101 can provide 1M/12 = 83,333 sample/s power
       60, // cutoff, means 1 minute for testnet and may need to change longer later
       1024, // diffAdjDivisor
       100, // treasuryShare, means 1%
@@ -17,9 +17,9 @@ async function main() {
     startTime, // startTime
     2000000000000, // storageCost - 2000Gwei forever per blob - https://ethresear.ch/t/ethstorage-scaling-ethereum-storage-via-l2-and-da/14223/6#incentivization-for-storing-m-physical-replicas-1
     340282365167313208607671216367074279424n, // dcfFactor, it mean 0.85 for yearly discount
-    1048576, // nonceLimit 1024 * 1024 = 1M samples and finish sampling in 1.3s with IO rate 6144 MB/s
+    1048576, // nonceLimit 1024 * 1024 = 1M samples and finish sampling in 1.3s with IO rate 6144 MB/s: 4k * 2(random checks) / 6144 = 1.3s
     "0x0000000000000000000000000000000000000000", // treasury
-    200000000000000, // prepaidAmount - ~ 100 blob cost, and for 1G data, it is about 100 / 8192 = 0.012
+    8192000000000000, // prepaidAmount - ~ 50% discount, 4096 blob cost for 1G data
     { gasPrice: 30000000000 }
   );
   await storageContract.deployed();
