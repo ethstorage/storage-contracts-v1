79e221c9eec66740d109b5f32a136024620a9ff8
syntrust <lundeng@quarkchain.org>
2023-11-03 10:56:54 +0800

    index height

--- contracts/EthStorageContractCelestia.sol
+++ contracts/EthStorageContractCelestia.sol
@@ -14,10 +14,10 @@ contract EthStorageContractCelestia is EthStorageContract {
         uint256 _prepaidAmount
     ) EthStorageContract(_config, _startTime, _storageCost, _dcfFactor, _nonceLimit, _treasury, _prepaidAmount) {}
 
-    event PutBlobToCelestia(uint256 indexed kvIdx, uint256 indexed kvSize, bytes32 indexed dataHash, uint256 height);
+    event BlobPublished(uint256 indexed kvIdx, uint256 indexed height, bytes32 indexed dataHash, uint256 kvSize);
 
-    function putBlob(bytes32 key, bytes32 commitment, uint256 size, uint256 height) public payable {
+    function publishBlob(bytes32 key, bytes32 commitment, uint256 size, uint256 height) public payable {
         uint256 kvIdx = _putInternal(key, commitment, size);
-        emit PutBlobToCelestia(kvIdx, size, commitment, height);
+        emit BlobPublished(kvIdx, height, commitment, size);
     }
 }
