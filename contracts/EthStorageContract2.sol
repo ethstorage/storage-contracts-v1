855a0a74104d8e7caad2a8b4bd90f551c1d0f34d
Qiang Zhu <freedostudio@gmail.com>
2024-01-16 21:15:32 +0800

    check encodingKey and xIn

--- contracts/EthStorageContract2.sol
+++ contracts/EthStorageContract2.sol
@@ -7,14 +7,56 @@ import "./Decoder2.sol";
 
 contract EthStorageContract2 is EthStorageContract, Decoder2 {
 
-    function decodeSample(
-        uint256[] memory masks,
-        bytes calldata decodeProof
-    ) public view returns (bool) {
+    function getEncodingKey(uint256 kvIdx, address miner) internal view returns (uint256) {
+        return uint256(keccak256(abi.encode(kvMap[idxMap[kvIdx]].hash, miner, kvIdx)));
+    }
+
+    function getXIn(uint256 sampleIdx) internal view returns (uint256) {
+        return modExp(ruBn254, sampleIdx, modulusBn254);
+    }
+
+    function _decodeSample(bytes calldata decodeProof, uint[6] memory _pubSignals) internal view returns (bool) {
         (uint[2] memory pA, uint[2][2] memory pB, uint[2] memory pC) = abi.decode(decodeProof, (uint[2], uint[2][2], uint[2]));
         // verifyProof uses the opcode 'return', so if we call verifyProof directly, it will lead to a compiler warning about 'unreachable code' 
-        // and causes the caller function return directly
-        return this.verifyProof(pA, pB, pC, [masks[0], masks[1]]);
+        // and causes the caller function return directly        
+        return this.verifyProof(pA, pB, pC, _pubSignals);
+    }
+
+    function decodeSample(
+        uint256[] memory masks,
+        uint256[] memory kvIdxs,
+        uint256[] memory sampleIdxs,
+        address miner,
+        bytes calldata decodeProof
+    ) public view returns (bool) {
+        return _decodeSample(decodeProof, [
+            getEncodingKey(kvIdxs[0], miner),
+            getEncodingKey(kvIdxs[1], miner),
+            getXIn(sampleIdxs[0]),
+            getXIn(sampleIdxs[1]),
+            masks[0],
+            masks[1]
+        ]);
+    }
+
+    function _checkSample(
+        uint256 startShardId, 
+        uint256 rows, 
+        bytes32 hash0,
+        bytes32 encodedSamples,
+        uint256 masks,
+        bytes calldata inclusiveProofs
+    ) internal view returns (bytes32, uint256, uint256) {
+        (uint256 kvIdx, uint256 sampleIdxInKv) = getSampleIdx(rows, startShardId, hash0);
+                
+        PhyAddr memory kvInfo = kvMap[idxMap[kvIdx]];
+
+        require(
+            checkInclusive(kvInfo.hash, sampleIdxInKv, masks ^ uint256(encodedSamples), inclusiveProofs),
+            "invalid samples"
+        );
+        hash0 = keccak256(abi.encode(hash0, encodedSamples));
+        return (hash0, kvIdx, sampleIdxInKv);
     }
 
     function verifySamples(
@@ -33,18 +75,20 @@ contract EthStorageContract2 is EthStorageContract, Decoder2 {
         // calculate the number of samples range of the sample check
         uint256 rows = 1 << (shardEntryBits + sampleLenBits);
 
+        uint[] memory kvIdxs = new uint[](randomChecks);
+        uint[] memory sampleIdxs = new uint[](randomChecks);
         for (uint256 i = 0; i < randomChecks; i++) {
-            (uint256 kvIdx, uint256 sampleIdxInKv) = getSampleIdx(rows, startShardId, hash0);
-            PhyAddr memory kvInfo = kvMap[idxMap[kvIdx]];
-
-            require(
-                checkInclusive(kvInfo.hash, sampleIdxInKv, masks[i] ^ uint256(encodedSamples[i]), inclusiveProofs[i]),
-                "invalid samples"
+            (hash0, kvIdxs[i], sampleIdxs[i]) = _checkSample(
+                startShardId, 
+                rows, 
+                hash0, 
+                encodedSamples[i], 
+                masks[i], 
+                inclusiveProofs[i]
             );
-            hash0 = keccak256(abi.encode(hash0, encodedSamples[i]));
         }
 
-        require(decodeSample(masks, decodeProof[0]), "decode failed");
+        require(decodeSample(masks, kvIdxs, sampleIdxs, miner, decodeProof[0]), "decode failed");
         return hash0;
     }
-}
+} 
