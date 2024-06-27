7dc0c4b9191431faa682d3921b00c4306a214c87
qiang <freedostudio@gmail.com>
2024-06-27 18:27:50 +0800

    implementation

--- contracts/EthStorageUpgradeableProxy.sol
+++ contracts/EthStorageUpgradeableProxy.sol
@@ -4,10 +4,17 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
 
 contract EthStorageUpgradeableProxy is TransparentUpgradeableProxy {
-
-    constructor(address _logic, address initialOwner, bytes memory _data) payable TransparentUpgradeableProxy(_logic, initialOwner, _data) {}
+    constructor(
+        address _logic,
+        address initialOwner,
+        bytes memory _data
+    ) payable TransparentUpgradeableProxy(_logic, initialOwner, _data) {}
 
     function admin() public view virtual returns (address) {
         return ERC1967Utils.getAdmin();
+    }
+
+    function implementation() public view virtual returns (address) {
+        return ERC1967Utils.getImplementation();
     }
 }
