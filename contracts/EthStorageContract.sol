655da0270a01093afc500fd38bbecc7437ec42dd
syntrust <lundeng@quarkchain.org>
2025-06-04 18:47:20 +0800

    separate decode per sample from EthStorageContract

--- contracts/EthStorageContract.sol
+++ contracts/EthStorageContract.sol
@@ -2,14 +2,13 @@
 pragma solidity 0.8.28;
 
 import "./StorageContract.sol";
-import "./zk-verify/Decoder.sol";
 import "./libraries//BinaryRelated.sol";
 import "./Interfaces/ISemver.sol";
 
 /// @custom:proxied
 /// @title EthStorageContract
 /// @notice EthStorage Contract that using EIP-4844 BLOB
-contract EthStorageContract is StorageContract, Decoder, ISemver {
+abstract contract EthStorageContract is StorageContract, ISemver {
     /// @notice The modulus for the BLS curve
     uint256 internal constant MODULUS_BLS = 0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001;
 
@@ -108,27 +107,6 @@ contract EthStorageContract is StorageContract, Decoder, ISemver {
         }
     }
 
-    /// @notice Verify the mask is correct
-    /// @param _proof The ZK proof
-    /// @param _encodingKey The encoding key
-    /// @param _sampleIdxInKv The sample index in the KV
-    /// @param _mask The mask of the sample
-    /// @return The result of the verification
-    function decodeSample(Proof memory _proof, uint256 _encodingKey, uint256 _sampleIdxInKv, uint256 _mask)
-        public
-        view
-        returns (bool)
-    {
-        uint256 xBn254 = _modExp(RU_BN254, _sampleIdxInKv, MODULUS_BN254);
-
-        uint256[] memory input = new uint256[](3);
-        // TODO: simple hash to curve mapping
-        input[0] = _encodingKey % MODULUS_BN254;
-        input[1] = xBn254;
-        input[2] = _mask;
-        return (verifyDecoding(input, _proof) == 0);
-    }
-
     /// @notice Check the decoded data is included in the BLOB corresponding to on-chain datahashes
     /// @param _dataHash The data hash
     /// @param _sampleIdxInKv The sample index in the KV
@@ -154,35 +132,6 @@ contract EthStorageContract is StorageContract, Decoder, ISemver {
         return evalY == _decodedData;
     }
 
-    /// @notice Decode the sample and check the decoded sample is included in the BLOB corresponding to on-chain datahashes
-    /// @param _kvIdx The index of the KV pair
-    /// @param _sampleIdxInKv The sample index in the KV
-    /// @param _miner The miner address
-    /// @param _encodedData The encoded sample data
-    /// @param _mask The mask of the sample
-    /// @param _inclusiveProof The inclusive proof
-    /// @param _decodeProof The decode proof
-    /// @return The result of the check
-    function decodeAndCheckInclusive(
-        uint256 _kvIdx,
-        uint256 _sampleIdxInKv,
-        address _miner,
-        bytes32 _encodedData,
-        uint256 _mask,
-        bytes calldata _inclusiveProof,
-        bytes calldata _decodeProof
-    ) public view virtual returns (bool) {
-        PhyAddr memory kvInfo = kvMap[idxMap[_kvIdx]];
-        Proof memory proof = abi.decode(_decodeProof, (Proof));
-        // BLOB decoding check
-        if (!decodeSample(proof, uint256(keccak256(abi.encode(kvInfo.hash, _miner, _kvIdx))), _sampleIdxInKv, _mask)) {
-            return false;
-        }
-
-        // Inclusive proof of decodedData = mask ^ encodedData
-        return checkInclusive(kvInfo.hash, _sampleIdxInKv, _mask ^ uint256(_encodedData), _inclusiveProof);
-    }
-
     /// @notice Get the sample index
     /// @param _rows The sample count per shard
     /// @param _startShardId The start shard ID
@@ -199,38 +148,6 @@ contract EthStorageContract is StorageContract, Decoder, ISemver {
         uint256 kvIdx = sampleIdx >> SAMPLE_LEN_BITS;
         uint256 sampleIdxInKv = sampleIdx % (1 << SAMPLE_LEN_BITS);
         return (kvIdx, sampleIdxInKv);
-    }
-
-    /// @inheritdoc StorageContract
-    function verifySamples(
-        uint256 _startShardId,
-        bytes32 _hash0,
-        address _miner,
-        bytes32[] memory _encodedSamples,
-        uint256[] memory _masks,
-        bytes[] calldata _inclusiveProofs,
-        bytes[] calldata _decodeProof
-    ) public view virtual override returns (bytes32) {
-        require(_encodedSamples.length == RANDOM_CHECKS, "EthStorageContract: data length mismatch");
-        require(_masks.length == RANDOM_CHECKS, "EthStorageContract: masks length mismatch");
-        require(_inclusiveProofs.length == RANDOM_CHECKS, "EthStorageContract: proof length mismatch");
-        require(_decodeProof.length == RANDOM_CHECKS, "EthStorageContract: decodeProof length mismatch");
-
-        // calculate the number of samples range of the sample check
-        uint256 rows = 1 << (SHARD_ENTRY_BITS + SAMPLE_LEN_BITS);
-
-        for (uint256 i = 0; i < RANDOM_CHECKS; i++) {
-            (uint256 kvIdx, uint256 sampleIdxInKv) = getSampleIdx(rows, _startShardId, _hash0);
-
-            require(
-                decodeAndCheckInclusive(
-                    kvIdx, sampleIdxInKv, _miner, _encodedSamples[i], _masks[i], _inclusiveProofs[i], _decodeProof[i]
-                ),
-                "EthStorageContract: invalid samples"
-            );
-            _hash0 = keccak256(abi.encode(_hash0, _encodedSamples[i]));
-        }
-        return _hash0;
     }
 
     /// @notice Write a large value to KV store.  If the KV pair exists, overrides it.
