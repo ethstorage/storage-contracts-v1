a22071f9196f524188c8affb3f220dddb4a48746
syntrust <lundeng@quarkchain.org>
2025-06-09 18:17:12 +0800

    fix build warns

--- contracts/EthStorageUpgradeableProxy.sol
+++ contracts/EthStorageUpgradeableProxy.sol
@@ -9,6 +9,10 @@ contract EthStorageUpgradeableProxy is TransparentUpgradeableProxy {
         TransparentUpgradeableProxy(_logic, initialOwner, _data)
     {}
 
+    receive() external payable virtual {
+        _fallback();
+    }
+
     function admin() public view virtual returns (address) {
         return ERC1967Utils.getAdmin();
     }
