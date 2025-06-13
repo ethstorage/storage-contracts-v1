2b40ff848e26760651caec9e2be756d5b27ffaaf
syntrust <lundeng@quarkchain.org>
2025-06-13 19:31:16 +0800

    restore prod values

--- script/DeployL2.s.sol
+++ script/DeployL2.s.sol
@@ -14,17 +14,17 @@ contract DeployL2 is Script {
     function run() external {
         // Read configuration parameters from environment variables (or use defaults)
         StorageContract.Config memory config = StorageContract.Config({
-            maxKvSizeBits: vm.envOr("MAX_KV_SIZE_BITS", uint256(17)),
-            shardSizeBits: vm.envOr("SHARD_SIZE_BITS", uint256(20)),
+            maxKvSizeBits: vm.envOr("MAX_KV_SIZE_BITS", uint256(17)), // 131072
+            shardSizeBits: vm.envOr("SHARD_SIZE_BITS", uint256(39)), // ~ 512G
             randomChecks: vm.envOr("RANDOM_CHECKS", uint256(2)),
-            cutoff: vm.envOr("CUTOFF", uint256(7200)),
+            cutoff: vm.envOr("CUTOFF", uint256(7200)), // cutoff = 2/3 * target internal (3 hours), 3 * 3600 * 2/3
             diffAdjDivisor: vm.envOr("DIFF_ADJ_DIVISOR", uint256(32)),
-            treasuryShare: vm.envOr("TREASURY_SHARE", uint256(100))
+            treasuryShare: vm.envOr("TREASURY_SHARE", uint256(100)) // 1%
         });
+        uint256 storageCost = vm.envOr("STORAGE_COST", uint256(570000000000000000)); // storage cost per blob - https://ethresear.ch/t/ethstorage-scaling-ethereum-storage-via-l2-and-da/14223/6#incentivization-for-storing-m-physical-replicas-1
+        uint256 dcfFactor = vm.envOr("DCF_FACTOR", uint256(340282366367469178095360967382638002176)); // 0.95 for yearly discount
+        uint256 updateLimit = vm.envOr("UPDATE_LIMIT", uint256(90)); // 45 blobs/s according to sync/encoding test, times block interval of L2
 
-        uint256 storageCost = vm.envOr("STORAGE_COST", uint256(570000000000000000)); // 0.57 ETH in wei
-        uint256 dcfFactor = vm.envOr("DCF_FACTOR", uint256(340282366367469178095360967382638002176));
-        uint256 updateLimit = vm.envOr("UPDATE_LIMIT", uint256(90));
         uint256 startTime = block.timestamp;
 
         // Get deployer address
@@ -39,9 +39,9 @@ contract DeployL2 is Script {
         // Prepare initialization data
         bytes memory data = abi.encodeWithSelector(
             impl.initialize.selector,
-            vm.envOr("MINIMUM_DIFF", uint256(943718)), // minimumDiff
-            vm.envOr("PREPAID_AMOUNT", uint256(1195376640000000000000000)), // prepaidAmount
-            vm.envOr("NONCE_LIMIT", uint256(1048576)), // nonceLimit
+            vm.envOr("MINIMUM_DIFF", uint256(94371840)), // minimumDiff 0.1 * 3 * 3600 * 1024 * 1024 / 12 = 94371840 for 0.1 replicas that can have 1M IOs in one epoch
+            vm.envOr("PREPAID_AMOUNT", uint256(1195376640000000000000000)), // prepaidAmount - 50% * 2^39 / 131072 * 570000000000000000, it also means around 1,200,000 QKC for half of the shard
+            vm.envOr("NONCE_LIMIT", uint256(1048576)), // nonceLimit 1024 * 1024 = 1M samples and finish sampling in 1.3s with IO rate 6144 MB/s: 4k * 2(random checks) / 6144 = 1.3s
             deployer, // treasuryAddress
             deployer // ownerAddress
         );
@@ -50,7 +50,7 @@ contract DeployL2 is Script {
         EthStorageUpgradeableProxy proxy = new EthStorageUpgradeableProxy(address(impl), deployer, data);
 
         // Fund the proxy contract
-        payable(address(proxy)).transfer(0.001 ether);
+        payable(address(proxy)).transfer(5000 ether); // fund 5000 qkc into the storage contract to give reward for empty mining
 
         vm.stopBroadcast();
 
