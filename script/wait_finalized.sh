1245e8c657ab1f0638e443fba34b442ca02b59db
ping-ke <molaokp@gmail.com>
2025-08-27 14:19:48 +0800

    fix

--- script/wait_finalized.sh
+++ script/wait_finalized.sh
@@ -7,15 +7,15 @@ RPC_URL=${RPC_URL_L2}
 
  LATEST_BLOCK_NUMBER=$(cast bn latest -r "$RPC_URL")
 
-echo "Waiting for finalized block number to exceed DEPLOYED_BLOCK: $LATEST_BLOCK_NUMBER"
+echo "Waiting for finalized block number to exceed LATEST_BLOCK_NUMBER: $LATEST_BLOCK_NUMBER"
 
 while true; do
   FINALIZED_BLOCK_NUMBER=$(cast bn finalized -r "$RPC_URL")
 
   echo "Current finalized block number: $FINALIZED_BLOCK_NUMBER"
 
-  if [ "$FINALIZED_BLOCK_NUMBER" -ge "$DEPLOYED_BLOCK" ]; then
-    echo "Finalized block number $FINALIZED_BLOCK_NUMBER exceeds or equals DEPLOYED_BLOCK $DEPLOYED_BLOCK"
+  if [ "$FINALIZED_BLOCK_NUMBER" -ge "$LATEST_BLOCK_NUMBER" ]; then
+    echo "Finalized block number $FINALIZED_BLOCK_NUMBER exceeds or equals LATEST_BLOCK_NUMBER $LATEST_BLOCK_NUMBER"
     break
   fi
 
