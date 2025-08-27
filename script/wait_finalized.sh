b88a5c1bca0faaf40184683741b614ac6db0c7dc
ping-ke <molaokp@gmail.com>
2025-08-27 14:16:55 +0800

    fix

--- script/wait_finalized.sh
+++ script/wait_finalized.sh
@@ -2,16 +2,12 @@
 set -e
 
 source .env
-source $(ls -t deployments/EthStorageContractM2L2*.txt | head -n 1)
 
 RPC_URL=${RPC_URL_L2}
 
-if [ -z "$DEPLOYED_BLOCK" ]; then
-  echo "Please set DEPLOYED_BLOCK environment variable"
-  exit 1
-fi
+ LATEST_BLOCK_NUMBER=$(cast bn latest -r "$RPC_URL")
 
-echo "Waiting for finalized block number to exceed DEPLOYED_BLOCK: $DEPLOYED_BLOCK"
+echo "Waiting for finalized block number to exceed DEPLOYED_BLOCK: $LATEST_BLOCK_NUMBER"
 
 while true; do
   FINALIZED_BLOCK_NUMBER=$(cast bn finalized -r "$RPC_URL")
