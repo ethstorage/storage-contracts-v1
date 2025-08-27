9fe2909dbb8fcccd483109fac0f1b3e8a90a372a
ping-ke <molaokp@gmail.com>
2025-08-27 14:20:50 +0800

    small change

--- script/wait_finalized.sh
+++ script/wait_finalized.sh
@@ -5,7 +5,7 @@ source .env
 
 RPC_URL=${RPC_URL_L2}
 
- LATEST_BLOCK_NUMBER=$(cast bn latest -r "$RPC_URL")
+LATEST_BLOCK_NUMBER=$(cast bn latest -r "$RPC_URL")
 
 echo "Waiting for finalized block number to exceed LATEST_BLOCK_NUMBER: $LATEST_BLOCK_NUMBER"
 
